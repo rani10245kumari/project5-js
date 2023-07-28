a1616cc58a4379747c17245fa030bfd8f212fe27
rani10245kumari <131008023+rani10245kumari@users.noreply.github.com>
2023-07-28 19:46:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,26 +17,36 @@ function showAddTask() {
     }
 
 }
-function hideAddTask() {
-    //const addTask = document.getElementById('pop')
-    addTask.setAttribute("class", "popupbox   hide");
-    parent.classList.remove("parent");
-}
-
-
-
 //--------------hide pop up box------
 function hideAddTask() {
-    addTask.classList.add("hide")
-    parent.classList.remove('blur-page');
-    visiblee = false;
+    /* const addTask = document.getElementById('pop')
+     addTask.setAttribute("class", "popupbox   hide");
+     parent.classList.remove("parent");
+     parent.classList.remove('blur-page');
+     visiblee = false;
+     addTask.classList.add("hide")*/
+    pop.remove()
 
 }
+
+
+
+
+
 
 //---------------creating card by js---------
 
 const containerCard = document.getElementById("cardcontainer")
 const newCardName = document.getElementById("cardName")
+let selectCard = document.getElementById("selectedcard")
+let navbar = document.querySelector("header")
+let blankspace = document.getElementsByClassName("blank")
+let backbutton = document.getElementsByClassName("backbutton")
+let plussign = document.getElementsByClassName("plus2")
+let button3 = document.getElementsByClassName("button3")
+
+
+
 function addCard() {
     var newCard = document.createElement("div")
     var cardTitle = document.createElement("h2")
@@ -66,6 +76,18 @@ function addCard() {
     deleteButton.innerHTML = "delete";
     addItem.innerHTML = "+"
 
+    /*------creating event listner----*/
+    addItem.addEventListener("click", () => {
+        newCard.style.height = "auto";
+
+        popup2(itemList)
+    })
+    deleteButton.addEventListener("click", () => {
+        newCard.remove();
+
+        popup2(itemList)
+    })
+
     /*  deleteButton.style = `
       color:rgb(109, 5, 45);
       background-color:antiquewhite;
@@ -87,18 +109,58 @@ function addCard() {
       border-radius:8px;
       `;
   */
-
+    /*-------------------itemlist popup----------*/
 
     function popup2(itemList) {
-        var head = document.createElement("h3");
-        itemList.appendChild(head);
-        let body = document.querySelector('body')
         var pop3 = document.createElement("div");
+
+        var h2 = document.createElement("h2")
+        let input = document.createElement("input")
+        let button4 = document.createElement("button")
+        let button5 = document.createElement("button")
+        let body = document.querySelector('body')
+
         pop3.classList.add("pop3")
         pop3.style.display = "block";
-        var h2 = document.createElement("h2")
         h2.innerText = "Add New List";
-        let input = document.createElement("input")
-        input.setAttribute.("id", "newCardName")
+        input.setAttribute("id", "newCardName")
+        button4.classList.add("button2");
+        button5.innerText = "Add"
+        button4.classList.add("button3");
+        button5.innerText = "Close"
+
+
+        button4.addEventListener("click", function () {
+            var item = document.createElement('div')
+            item.innerText = input.value
+            itemList.appendChild(item)
+        })
+
+        button5.addEventListener("click", function () {
+            pop3.remove()
+        })
+
+
+        body.appendChild(pop3);
+        pop3.appendChild(h2)
+        pop3.appendChild(input)
+        pop3.appendChild(button4)
+        pop3.appendChild(button5)
+
     }
+    /*-------on click of h2-------------*/
+    cardTitle.addEventListener("click", function () {
+        containerCard.style.display = "none"
+        selectCard.style.display = "block"
+        selectCard.appendChild(newCard)
+        selectCard.style.display = "flex"
+        header.style.display = "block"
+        parent.style.display = "none"
+        button3[0].style.display = "none"
+        blankspace[0].innerText = cardTitle.innerText
+
+
+
+    })
+
 }
